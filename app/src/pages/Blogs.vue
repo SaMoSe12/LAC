649063ff0bb884475b6403663735a8aefc32b4eb
SaMoSe12 <santiago.moguel.secades@gmail.com>
2024-08-02 14:21:30 -0600

    small changes 4

--- app/src/pages/Blogs.vue
+++ app/src/pages/Blogs.vue
@@ -1,7 +1,7 @@
 <script scoped setup lang="ts">
-
+    import TitleStrip from '../components/TitleStrip.vue'
 </script>
 <template class="p-2 m-2">
-
+    <TitleStrip>Ideas</TitleStrip>
 </template>
 <style scoped></style>
