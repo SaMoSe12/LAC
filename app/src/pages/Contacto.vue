ed5b1204ac87fe8ff98a2978bc0dbfe48cf81205
SaMoSe12 <santiago.moguel.secades@gmail.com>
2024-08-02 15:36:49 -0600

    small changes 5

--- app/src/pages/Contacto.vue
+++ app/src/pages/Contacto.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-
+import TitleStrip from '../components/TitleStrip.vue'
 </script>
 <template>
-    
+    <TitleStrip><template #titleString>Contacto</template></TitleStrip>
 </template>
 <style scoped></style>
